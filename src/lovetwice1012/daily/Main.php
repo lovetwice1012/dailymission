67d3966f3c82fa0b104577f2cbdd3491360b4c7b
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-06-04 09:58:13 +0900

    Update Main.php

--- src/lovetwice1012/daily/Main.php
+++ src/lovetwice1012/daily/Main.php
@@ -22,19 +22,19 @@ class Main extends PluginBase implements Listener
     public $money;
     public function onEnable()
     {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);        
         $this->load();
-        $this->plugin = $this;
-        
-        
-    }
-    
+        $this->plugin = $this;               
+    }    
     public function load()
     {
         date_default_timezone_set('Asia/Tokyo');
         $this->data = new Config($this->getDataFolder() . "Data.yml", Config::YAML);
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
         $this->money = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
+        if(!$this->config->exists("money")){
+        $this->config->set("money",10000)
+        }
     }
     public function isToday()
     {
@@ -44,18 +44,13 @@ class Main extends PluginBase implements Listener
             $this->data->set("date", date("Y/m/d"));
             $Bdata = $this->data->getAll();
             foreach ($Bdata as $key => $value) {
-                if ($key != "date" && $key != "nowmission") {
-                    
-                    
+                if ($key != "date" && $key != "nowmission") {                    
                     $this->data->set($key, 0);
                     $this->data->save();
-                }
-                
+                }               
             }
             $rand  = rand(1, 4);
-            $rand2 = rand(1, 64);
-            
-            
+            $rand2 = rand(1, 64);              
             $Ajob = "";
             $this->data->set($key, 0);
             switch ($rand) {
@@ -71,13 +66,10 @@ class Main extends PluginBase implements Listener
                 case 4:
                     $Ajob = "kill";
                     break;
-            }
-            
+            }           
             $this->data->set("nowmission", $Ajob);
-            $this->data->set("checkpoint", $rand2);
-            
-            $this->data->save();
-            
+            $this->data->set("checkpoint", $rand2);           
+            $this->data->save();         
             return true;
         }
     }
@@ -95,8 +87,7 @@ class Main extends PluginBase implements Listener
             return true;
         } else {
             return false;
-        }
-        
+        }      
     }
     public function isFinish($player)
     {
@@ -104,65 +95,50 @@ class Main extends PluginBase implements Listener
             return true;
         } else {
             return false;
-        }
-        
+        }        
     }
     public function addProgress($player, $type)
     {
         $this->isToday();
         if ($this->isNowmission($type)) {
             if ($this->data->exists($player->getName())) {
-                $this->data->set($player->getName(), $this->data->get($player->getName()) + 1);
-                
+                $this->data->set($player->getName(), $this->data->get($player->getName()) + 1);             
             } else {
-                $this->data->set($player->getName(), 1);
-                
-                
+                $this->data->set($player->getName(), 1);                            
             }
-            $player->sendTip("デイリーミッションの進行度:".$this->data->get("nowmission")."を".$this->data->get("checkpoint")."回中".$this->data->get($player->getName())."回行いました。");
-            
-            $this->data->save();
-            
+            if (!$this->isFinish($player)) {
+                $player->sendTip("デイリーミッションの進行度:".$this->data->get("nowmission")."を".$this->data->get("checkpoint")."回中".$this->data->get($player->getName())."回行いました。");
+            }
+            $this->data->save();          
             if ($this->checkProgress($player)) {
                 //ここにデイリーミッションクリアしたことを通知するコード入れてね
-                $player->addTitle("デイリーミッションクリア！1万円プレゼント！", "明日もがんばってね！");
-                $this->money->addMoney($player, 10000);
-            }
-            
-            
+                $player->addTitle("デイリーミッションクリア！".$this->config->get("money")."円プレゼント！", "明日もがんばってね！");
+                $this->money->addMoney($player, $this->config->get("money"));
+            }                        
         }else{
             if ($this->isFinish($player)) {
                 
             }else{
-        $player->sendTip("今日のデイリーミッションは".$this->data->get("nowmission")."を".$this->data->get("checkpoint")."回することです。");
+                $player->sendTip("今日のデイリーミッションは".$this->data->get("nowmission")."を".$this->data->get("checkpoint")."回することです。");
             }
-            }
+        }
         $this->data->save();
     }
     public function onBreak(BlockBreakEvent $event)
     {
         $player = $event->getPlayer();
         $block  = $event->getBlock();
-        if ($block instanceof Wood or $block instanceof Wood2) {
-            
-                $this->plugin->addProgress($player, "wood");
-                
-            
-        } elseif ($block instanceof Diamond or $block instanceof DiamondOre or $block instanceof Iron or $block instanceof IronOre or $block instanceof Gold or $block instanceof GoldOre or $block instanceof Emerald or $block instanceof EmeraldOre or $block instanceof Stone or $block instanceof Cobblestone or $block instanceof Redstone or $block instanceof RedstoneOre or $block instanceof Coal or $block instanceof CoalOre or $block instanceof Lapis or $block instanceof LapisOre) {
-            
-                $this->plugin->addProgress($player, "mine");
-                
-            
+        if ($block instanceof Wood or $block instanceof Wood2) {            
+                $this->plugin->addProgress($player, "wood");                           
+        } elseif ($block instanceof Diamond or $block instanceof DiamondOre or $block instanceof Iron or $block instanceof IronOre or $block instanceof Gold or $block instanceof GoldOre or $block instanceof Emerald or $block instanceof EmeraldOre or $block instanceof Stone or $block instanceof Cobblestone or $block instanceof Redstone or $block instanceof RedstoneOre or $block instanceof Coal or $block instanceof CoalOre or $block instanceof Lapis or $block instanceof LapisOre) {           
+                $this->plugin->addProgress($player, "mine");           
         }
     }
     
     public function onPlace(BlockPlaceEvent $event)
     {
         $player = $event->getPlayer();
-       
-            $this->plugin->addProgress($player, "build");
-            
-        
+        $this->plugin->addProgress($player, "build");                   
     }
     public function onKill(PlayerDeathEvent $event)
     {
@@ -172,8 +148,7 @@ class Main extends PluginBase implements Listener
             $player = $cause->getDamager();
             if ($player instanceof Player) {
                 if ($entity instanceof Player) {
-                    $this->plugin->addProgress($player, "kill");
-                    
+                    $this->plugin->addProgress($player, "kill");                    
                 }
             }
         }
